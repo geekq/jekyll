754ec02652cd963ff178b5e95700841e24ca5e44
Damien Tanner <damien@new-bamboo.co.uk>
2009-02-16 22:26:00 +0000

    liquid filter to pull in snippets from gists.github.com into posts

--- lib/jekyll/filters.rb
+++ lib/jekyll/filters.rb
@@ -39,5 +39,9 @@ module Jekyll
       end
     end
 
+    def gist(id)
+      js = open("http://gist.github.com/#{id}.js").read
+      js.match(/document.write\('(<div.+)'\)/)[1].gsub(/\\"/, '"').gsub(/\\\//, '/').gsub(/\\n/, '')
+    end
   end  
 end
